4c8936de9329802613d0765b0591e1420bba26e2
威老 <qqq123026689@126.com>
2014-08-30 17:34:01 +0800

    remove "()"

--- console.js
+++ console.js
@@ -1,8 +1,8 @@
-// Console-Polyfill. MIT license.
+// console-js. MIT license.
+// @author Weilao
 (function (_console) {
-    "use strict";
     var method,
-        log = _console.log || new Function(),
+        log = _console.log || new Function,
         methods = 'debug,error,info,warn,dir,dirxml,table,trace,assert,count,markTimeline,profile,profileEnd,time,timeEnd,timeStamp,timeline,timelineEnd,group,groupCollapsed,groupEnd,clear'.split(',');
     while (method = methods.pop()) _console[method] = _console[method] || log;
     _console.memory = _console.memory || {};
