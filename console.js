3b53d45dddf9b0ec296e2fb9c58b40fe4f4e0e99
威老 <qqq123026689@126.com>
2014-09-04 19:15:51 +0800

    fixbug

--- console.js
+++ console.js
@@ -3,7 +3,7 @@
 (function (_console) {
     var method,
         log = _console.log || new Function,
-        methods = 'debug,error,info,warn,dir,dirxml,table,trace,assert,count,markTimeline,profile,profileEnd,time,timeEnd,timeStamp,timeline,timelineEnd,group,groupCollapsed,groupEnd,clear'.split(',');
+        methods = 'log,debug,error,info,warn,dir,dirxml,table,trace,assert,count,markTimeline,profile,profileEnd,time,timeEnd,timeStamp,timeline,timelineEnd,group,groupCollapsed,groupEnd,clear'.split(',');
     while (method = methods.pop()) _console[method] = _console[method] || log;
     _console.memory = _console.memory || {};
 })(window.console = window.console || {});
